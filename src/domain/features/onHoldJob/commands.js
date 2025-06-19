cc72b5db2ea96ec817b06736d242e79682575e16
clecoentddd <christophelecoent@gmail.com>
2025-06-19 21:27:15 +0200

    job on hold working

---
+++ src/domain/features/onHoldJob/commands.js
@@ -0,0 +1,17 @@
+// src/domain/features/onHoldJob/commands.js
+// Defines commands related to putting a job on hold.
+
+/**
+ * Factory function for creating a PutJobOnHoldCommand.
+ * This command is used to change the status of a pending job to "On Hold".
+ * @param {string} jobId - The ID of the job to put on hold.
+ * @param {string} heldByUserId - The ID of the user who put the job on hold.
+ * @param {string} reason - The reason for putting the job on hold.
+ * @returns {object} The PutJobOnHoldCommand object.
+ */
+export const PutJobOnHoldCommand = (jobId, heldByUserId, reason) => ({
+  type: 'PutJobOnHold', // Command type identifier
+  jobId,
+  heldByUserId,
+  reason,
+});
